637080cbc2b508850620530bf0706d3ef2cc1101
yuldoshov.shakhzod <shakhzodbek.yuldoshev@gmail.com>
2020-03-04 17:44:41 +0500

    touch working

--- src/App.js
+++ src/App.js
@@ -1,50 +1,86 @@
-import React from 'react'; import './App.css';import 'bootstrap/dist/css/bootstrap.min.css'
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-class Reservation extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isGoing: true,
-            numberOfGuests: 0
-        };
+class MiniFormik extends React.Component {
+    state = {
+        values: this.props.initialValues || {},
+        touched: {},
+        errors: {}
+    };
 
-        this.handleInputChange = this.handleInputChange.bind(this);
-    }
-
-    handleInputChange(event) {
+    handleChange = event => {
         const target = event.target;
         const value = target.type === 'checkbox' ? target.checked : target.value;
         const name = target.name;
+        event.persist();
+        this.setState(prevState => ({
+            values: {
+                ...prevState.values,
+                [name]: value
+            },
+        }));
+    };
+    handleBlur = event => {
+        const target = event.target;
 
-        this.setState({
-            [name]: value
-        });
-    }
+        const name = target.name;
+        event.persist();
+        this.setState(prevState => ({
+            touched: {
+                ...prevState.touched,
+                [name]: true,
+            },
+        }));
+    };
 
     render() {
+        return this.props.children({
+            ...this.state,
+            handleChange: this.handleChange,
+            handleBlur: this.handleBlur,
+        });
+    }
+}
+
+class Reservation extends React.Component {
+    render() {
         return (
-            <form className="border shadow rounded bg-transparent text-white p-5">
-                <label className="d-flex">
-                    Is going:
-                    <input
-                        className="form-control w-25"
-                        name="isGoing"
-                        type="checkbox"
-                        checked={this.state.isGoing}
-                        onChange={this.handleInputChange} />
-                </label>
-                <br />
-                <label className="w-100">
-                    Number of guests:
-                    <input
-                        className="form-control my-2"
-                        name="numberOfGuests"
-                        type="number"
-                        value={this.state.numberOfGuests}
-                        onChange={this.handleInputChange} />
-                </label>
-                <input type="submit" className="form-control bg-success border-success text-white"/>
-            </form>
+            <MiniFormik initialValues={{
+                isGoing: true,
+                numberOfGuests: 2,
+            }}>
+                {(props) => {
+                    const {values, errors, touched, handleChange, handleBLur} = props;
+                    return(
+                    <form className="border form_shadow rounded bg-transparent text-white p-5">
+                        <label className="d-flex justify-content-between">
+                            Is going:
+                            <input
+                                className="form-control "
+                                name="isGoing"
+                                type="checkbox"
+                                checked={values.isGoing}
+                                onChange={handleChange}
+                                onBlur={handleBLur}
+                            />
+                        </label>
+                        <br/>
+
+                        <label className="w-100">
+                            Number of guests:
+                            <input
+                                className="form-control my-2"
+                                name="numberOfGuests"
+                                type="number"
+                                value={values.numberOfGuests}
+                                onChange={handleChange}/>
+                        </label>
+                        <input type="submit" className="form-control bg-success border-success  text-white"/>
+                        <pre className="my-3 rounded p-3">{JSON.stringify(props, null, 2)}</pre>
+                    </form>
+                )}}
+            </MiniFormik>
         );
     }
 }
